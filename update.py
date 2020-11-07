429bc761d24991d85d7eb7d2efb639ee8081bfc2
Wade <40057831+WadeStack@users.noreply.github.com>
2020-11-07 23:54:14 +0800

    Update update.py

--- update.py
+++ update.py
@@ -2,9 +2,6 @@ import re
 import requests
 import xml.etree.ElementTree as ET
 
-feed = requests.get('https://WadeStack.github.io/atom.xml').text
-root = ET.fromstring(feed)
-nsfeed = {'nsfeed': 'http://www.w3.org/2005/Atom'}
 with open('README.md', 'w') as f:
     f.write(r'''
 ```python
